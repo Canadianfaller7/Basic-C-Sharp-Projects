13bd3cc290a810dc30cefc5b92a53b702128f62f
Canadianfaller7 <canadianfaller7@gmail.com>
2022-01-24 18:32:18 -0700

    Added new projects

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name?");
+            var username = Console.ReadLine();
+            Console.WriteLine($"\nHello {username}!");
+        }
+    }
+}
+ 
