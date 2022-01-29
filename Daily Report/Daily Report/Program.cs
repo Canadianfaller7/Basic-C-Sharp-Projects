bbe3e8ecd3d4d82da3075c573e0c1a529b2dd081
Spencer Merrill <canadianfaller7@gmail.com>
2022-01-28 22:13:27 -0700

    updated my code

--- Daily Report/Daily Report/Program.cs
+++ Daily Report/Daily Report/Program.cs
@@ -10,30 +10,31 @@ namespace Daily_Report
     {
         static void Main(string[] args)
         {
+            int pgNum = 0;
+            bool yOrn = true;
+            int hours = 0;
             Console.WriteLine("The Tech Academy.\n\nStudent Daily Report.\n\n");
             Console.WriteLine("What is your name?\n");
-            string name = Console.ReadLine().ToUpper();
+            Console.ReadLine().ToUpper();
             Console.WriteLine();
             Console.WriteLine("What course are you on?\n");
-            string course = Console.ReadLine().ToUpper();
+            Console.ReadLine().ToUpper();
             Console.WriteLine();
             Console.WriteLine("What page number?\n");
-            string pgNum = Console.ReadLine();
-            int num = Convert.ToInt32(pgNum);
+            pgNum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"\n");
-            string yOrn = Console.ReadLine();
-            bool answer = Convert.ToBoolean(yOrn);
+            yOrn = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine();
-            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
-            string experiences = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.\n");
+            Console.ReadLine();
             Console.WriteLine();
             Console.WriteLine("How man hours did you study today?");
-            string hours = Console.ReadLine();
-            int hoursStudied = Convert.ToInt32(hours);
+            hours = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
+
         }
     }
 }
