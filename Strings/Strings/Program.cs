f4bb7832b90aef64bdfec58db5eaae8718da850c
Spencer Merrill <canadianfaller7@gmail.com>
2022-02-02 21:42:20 -0700

    Added comments

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -24,13 +24,15 @@ namespace Strings
             //Console.WriteLine(name);
             //Console.ReadLine();
 
+            /* here I am concatenating these variables with more stirng to build a sentence and then printing to final code */
             string name = "Terran";
             string description = name + ", is my best friend and is the one who helped me get into coding";
             string hobbies = description + ".\nWe both love skating, coding, playing chess, and building gameboys together.\n";
             Console.WriteLine(hobbies);
 
         
-
+            /* here we are using the stringBuilder class to build a paragraph about myself without having to create multiple variables like
+             above and waste space in memory.*/
             StringBuilder sb = new StringBuilder();
             sb.Append("My name is Spencer, ");
             sb.Append("I am a junior software engineer.");
