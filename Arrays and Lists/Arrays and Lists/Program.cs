8bd17b15809578395cd6d412ef62bee27f99b7d1
Spencer Merrill <canadianfaller7@gmail.com>
2022-02-03 00:41:18 -0700

    First Commit

---
+++ Arrays and Lists/Arrays and Lists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        // here is a lengthy way to make an array
+        int[] numArray = new int[5];
+        numArray[0] = 5;
+        numArray[1] = 2;
+        numArray[2] = 10;
+        numArray[3] = 20;
+        numArray[4] = 200;
+
+        int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 }; // here is the short easier way to make an array
+
+        int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 }; // here is an even shorter and easier way to make an array
+        numArray2[5] = 650; // this changes the 600 above to 650
+
+        Console.WriteLine(numArray2[3]);
+
+        // here we will deal with lists
+
+        List<int> intList = new List<int>(); // this is a way to make lists
+        intList.Add(5); // here we add things to the list
+        intList.Add(2); 
+        intList.Add(10);
+        intList.Remove(10); // here we remove something from our list
+
+        List<string> stringList = new List<string>();
+        stringList.Add("Hello");
+        stringList.Add("Spencer");
+        stringList.Add("Kristel");
+        stringList.Remove("Hello");
+
+        Console.WriteLine(stringList[1]);
+
+        Console.WriteLine(intList[0]);
+
+        Console.ReadLine();
+        }
+    }
+
---
+++ ConsoleAppArray/ConsoleAppArray/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* here we make a new string array and then we ask the user to give us a number of 0-3 to reach into the array and print
+             the arrays value based on the number the user gave*/
+            string[] randomArray = { "Hello there!", "How are you today?", "I love coding!", "This is a random sentence!" };
+            Console.WriteLine("Give me a number between 0-3 to print something:\n");
+            int usrNum1 = Convert.ToInt32(Console.ReadLine()); // we convert their answer to an int from a string
+            /* I then run an if statement to make sure that the number the user gave isn't out of our index and there is actually something to
+             be printed or they will get told the value doesn't exist and then move onto the next question*/
+            if (usrNum1 > 3)
+            {
+                Console.WriteLine("\nSorry this value doesn't exist.");
+            }
+            else
+            {
+                Console.WriteLine(randomArray[usrNum1]);
+            }
+
+            // this is the same thing as above, but this will print a number
+            int[] numArray = { 1, 44, 77, 7, 25, 2340, 530, 233, 6856, 400, 5, 9 };
+            Console.WriteLine("\nGive me a number between 0-11 to print out a new number:\n");
+            int usrNum2 = Convert.ToInt32(Console.ReadLine());
+
+            if (usrNum2 > 11)
+            {
+                Console.WriteLine("\nSorry this value doesn't exist.");
+            }
+            else
+            {
+                Console.WriteLine(numArray[usrNum2]);
+            }
+
+            // this is basically the same as above, but here we are making a list.
+            List<string> list = new List<string>(); // this is how you make a string list (list) is the var name where we will add our values
+            list.Add("Here is the first string in the list"); // this is how you add values to the list
+            list.Add("Here is the second string in the list");
+            list.Add("Coding is the best thing ever!");
+            list.Add("Skateboarding is also the best thing ever!");
+
+            Console.WriteLine("\nGive me another number to get a string from the list I created:\n");
+            int usrNum3 = Convert.ToInt32(Console.ReadLine());
+
+            if (usrNum3 > 3)
+            {
+                Console.WriteLine("\nSorry this value doesn't exist");
+            }
+            else
+            {
+                Console.WriteLine(list[usrNum3]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
