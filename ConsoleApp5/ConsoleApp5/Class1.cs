7d40b0b19697edc8f0cdec26b144cdc88aee2343
Spencer Merrill <canadianfaller7@gmail.com>
2022-02-10 02:18:52 -0700

    First Commit

---
+++ ConsoleApp5/ConsoleApp5/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Class1
+    {
+        public void RandomMethod(int num1)// method that doesn't return anything but has a math operation going on
+        {
+            int dividNumber = num1 / 2;
+            Console.WriteLine(dividNumber);
+        }
+        public void Methodless() // method with no parameters
+        {
+            Console.WriteLine("This is a method with no parameters");
+        }
+        public void RandomMethod(string sentence) // overload method of our first method
+        {
+            Console.WriteLine(sentence);
+        }
+    }
+    public static class StaticClass // this is a static class( a class that hasn't been instantiated yet and we will then just call in our Main()
+    {
+        public static void Sentence()
+        {
+            Console.WriteLine("\nHello World!");
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 randomClass = new Class1(); // here we instantiated our class1 by making it into an object called randomClass
+
+            string randomSentence = "\nThis is the same method as the one with no paramter but we have now overloaded it with a parameter"; // this is for our overload method
+
+            randomClass.Methodless(); // this calls our method with no paramters and prints out a message only
+            randomClass.RandomMethod(randomSentence); // this is an overload method of our RandomMethod that takes in a string parameter instead of an int
+
+            Console.WriteLine("\nGive me a number:\n"); // this is for the original randomMethod method we originally made that has the int
+            int usrNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\n{usrNumber} divided by 2 equals:\n"); // inside our method we have a math operation going on and we tell the user what is happening
+            randomClass.RandomMethod(usrNumber); // this is how we use our method from our class 
+
+            StaticClass.Sentence(); // this is our StaticClass with our sentence inside and will be displayed to our screen. 
+            // this is how we can access our class without instantiating it
+
+            Console.ReadLine();
+        }
+    }
+}
