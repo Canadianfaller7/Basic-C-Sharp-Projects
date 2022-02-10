3a737cbd7457337f0425639fa1bef4cbc6ed394f
Spencer Merrill <canadianfaller7@gmail.com>
2022-02-09 18:58:37 -0700

    Fixed code, updated if else statement to try/catch block

--- ConsoleApp3/ConsoleApp3/Class1.cs
+++ ConsoleApp3/ConsoleApp3/Class1.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp3
 {
     internal class Class1
     {
-        public static int Number(int number1, int number2 = 7) // this is my method taking 2 int parameters but the second one is optional
+        public int Number(int number1, int number2 = 7) // this is my method taking 2 int parameters but the second one is optional
             // so if a second parameter isn't entered it will have a default value of 7
         {
             return number1 + number2;
--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -10,24 +10,30 @@ namespace ConsoleApp3
     {
         public static void Main(string[] args)
         {
+            Class1 class1 = new Class1();
+
             Console.WriteLine("Please provide me with 2 numbers.\n\nGive me your first number:\n"); // ask user to give first number
             int usrNum1 = Convert.ToInt32(Console.ReadLine());// save first number
-            Console.WriteLine("\nFor number 2 this is optional to provide or not: If you don't have a second number put 0.\n"); // ask for 2nd number and tell user it is only optional
-            int usrNum2 = Convert.ToInt32(Console.ReadLine()); // save second number
-            Console.WriteLine("\nThank you for giving us numbers!\n\nWe will now go ahead and add them together.\n");
 
-            if (usrNum2 == 0) // if user didn't have a second number they could put 0 and then the code below would run
+            try // this will run the program if the user inputs 2 numbers
+            {
+                Console.WriteLine("\nFor number 2 this is optional to provide or not: If you don't have a second number hit enter.\n"); // ask for 2nd number and tell user it is only optional
+                int usrNum2 = Convert.ToInt32(Console.ReadLine()); // save second number
+                Console.WriteLine("\nThank you for giving us numbers!\n\nWe will now go ahead and add them together.\n");
+
+                int addNumbers = class1.Number(usrNum1, usrNum2); // changes the second parameter from our method from 7 to whatever the user typed in
+                Console.WriteLine($"Your first number {usrNum1} + your second number {usrNum2} equals: {addNumbers}");
+            }
+            catch  (Exception) // if the user only inputs 1 number then we catch the error and make an exception and then run the code with the defualt value we set in our class
             {
                 Console.WriteLine("If you did not enter a second number, don't worry we have provided one for you!\n");
-                int addNum = Class1.Number(usrNum1); // this is running our method from our class with just 1 parameter taken and using the second as the default value of 7
+                int addNum = class1.Number(usrNum1); // this is running our method from our class with just 1 parameter taken and using the second as the default value of 7
                 Console.WriteLine("\nYour number {0} + 7 equals: {1}\n", usrNum1, addNum);
-            }
-            else // else if user added a second number of their choosing run this code
-            {
-                int addNumbers = Class1.Number(usrNum1, usrNum2); // changes the second parameter from our method from 7 to whatever the user typed in
-                Console.WriteLine($"Your first number {usrNum1} + your second number {usrNum2} equals: {addNumbers}");
             }
             Console.ReadLine();
         }
     }
 }
+
+
+
