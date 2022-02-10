3010586c0100279a68a60c435800fc8b82adc4da
Spencer Merrill <canadianfaller7@gmail.com>
2022-02-10 00:02:44 -0700

    First Commit

---
+++ ConsoleApp4/ConsoleApp4/Divide.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Divide // this is our class
+    {
+        public void getNumber(int num1, int num2) // we do void because we aren't returning the parameters
+        {
+            int divideNumbers = num1 / 2; // take first parameter and divide by 2
+            Console.WriteLine(divideNumbers); // display our math operation to screen
+            Console.WriteLine(num2); // display second parameter to screen only
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Divide divide = new Divide(); // here we instantiate our class we made and make it into an object
+
+            divide.getNumber(7, 12); // we then call the class here passing in 2 numbers for the parameters
+            divide.getNumber(num1: 64, num2: 22); // this is the same, but here we are specifying the parameters by name and then giving a value
+
+            //Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
