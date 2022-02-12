03b7c1d30c3712cb0513832abd9cab478a9cf4c3
Spencer Merrill <canadianfaller7@gmail.com>
2022-02-11 20:57:37 -0700

    Overloaded == and != operators to compare two employee id's to see if they are the same

--- ConsoleApp7/ConsoleApp7/Employee.cs
+++ ConsoleApp7/ConsoleApp7/Employee.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp7
 {
     public class Employee : Person, IQuittable // now we inherit everything from Person and Iquittable class
     {
+        public int Id { get; set; }
         public override void SayName() // implementing the method from our abstract class and telling it what to do when used
         {
            string fullName = firstName + " " + lastName; // made a var to combine both the first and last name properties
@@ -16,6 +17,21 @@ namespace ConsoleApp7
         public void Quit() // implementing the Quit method from our interface class
         {
             Console.WriteLine("I don't want to work today and just want to code"); // passing in this info to be written to console when method is called.
+        }
+        public  static bool operator == (Employee employee1, Employee employee2) // here we are overriding the == operator and passing in 2 parameters of employee's
+        {
+            if (employee1.Id == employee2.Id) // we then say if their Id's are equal to return true
+            {
+                return true;
+            }
+            else
+            {
+                return false; // if not return false
+            }
+        }
+        public static bool operator != (Employee employee1, Employee employee2) // we need to override the not equal operator as well to satisfy the override of the == operator
+        { 
+            return !(employee1 == employee2);  
         }
     }
 }
--- ConsoleApp7/ConsoleApp7/Program.cs
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -10,13 +10,23 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee(); // we instantiate the Employee class and make it an object
-            employee.firstName = "Sample"; // assign value to property
-            employee.lastName = "Student"; // assign value
-            employee.SayName(); // call method within the class
+            Employee employee1 = new Employee(); // we instantiate the Employee class and make it an object
+            employee1.firstName = "Sample"; // assign value to property
+            employee1.lastName = "Student"; // assign value
+            employee1.Id = 1234;
+            employee1.SayName(); // call method within the class
+
+            Employee employee2 = new Employee(); // we instantiate the Employee class and make it an object
+            employee2.firstName = "Student"; // assign value to property
+            employee2.lastName = "Sample"; // assign value
+            employee2.Id = 1235;
+            employee2.SayName(); // call method within the class
 
             IQuittable quit = new Employee(); // this is the use of polymorphism we made an object by calling the IQuittable class and making it equal to the Employee class
             quit.Quit(); // we then call the Interface method here
+
+            Console.WriteLine("\nAre these employees the same?\n");
+            Console.WriteLine(employee1 == employee2); // comparing the two employee's to see if they are the same or not.(we didn't need to do employee1.Id because we already did that in our class so here we can just write it the way it is)
 
             Console.ReadLine(); // display sayName method
         }
