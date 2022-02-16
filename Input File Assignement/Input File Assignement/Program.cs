775778ee2d12109f3b4c1bd28989d01fc4227bc8
Spencer Merrill <canadianfaller7@gmail.com>
2022-02-16 02:01:15 -0700

    saving user number to a file and then displaying the text in the file to the console

---
+++ Input File Assignement/Input File Assignement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input_File_Assignement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number:\n");
+            string usrNum = Console.ReadLine();
+            File.WriteAllText(@"D:\GitHub\Basic-C-Sharp-Projects\Input File Assignement\log.txt", usrNum);
+            Console.WriteLine("\nHere is the number that we saved:\n" + File.ReadAllText(@"D:\GitHub\Basic-C-Sharp-Projects\Input File Assignement\log.txt"));
+            Console.ReadLine();
+        }
+    }
+}
