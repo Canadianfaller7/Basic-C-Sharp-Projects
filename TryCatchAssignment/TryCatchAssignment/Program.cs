5d6a4c6999657a174363af3fadf6987b4c799fbf
Spencer Merrill <canadianfaller7@gmail.com>
2022-02-16 19:03:20 -0700

    Made an app that takes a persons age and prints the year they were born

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TryCatchAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            bool validAnswer = false;
+            int age = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("\nHow old are you going to be this year?\n");
+                validAnswer = int.TryParse(Console.ReadLine(), out age);
+
+                try
+                {
+                    DateTime now = DateTime.Now;
+                    if (!validAnswer)
+                    {
+                        Console.WriteLine("\nEnter in your actual age.");
+                    }
+                    else if (age <= 0)
+                    {
+                        throw new ArgumentException();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nBased on the age you entered, we caclulated that you were born this year: {now.Year - age}");
+                    }
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nEnter in a whole number only.");
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("\nYou may not enter 0 or a negative number! Please enter in your age.");
+                    Console.ReadLine();
+                    return;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nYou must have typed in something wrong and an error has occured.");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
