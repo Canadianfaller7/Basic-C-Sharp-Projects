353e25e288ba5fe720953fac11485942314b1cc2
Spencer Merrill <canadianfaller7@gmail.com>
2022-02-24 01:24:18 -0700

    first commit

---
+++ CodeFirstNewDatabase/CodeFirstNewDatabase/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstNewDatabase
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBrith { get; set; }
+        public Grade Grade { get; set; }
+    }
+}
